100c414c972254e9f49f3ed5d9c2b9b56d40a57f
James Lo <lo.jamess@gmail.com>
2018-11-13 17:21:56 -0500

    Secondary commit after initial setup.

---
+++ frontend/components/app.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+
+
+const App = () => (
+  <div>
+    <header>
+      <h1>Yawp!</h1>
+    </header>
+  </div>
+);
+
+export default App;
