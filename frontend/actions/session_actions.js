1ba9374c487d921f77bc73d717702fbd280ca29d
James Lo <lo.jamess@gmail.com>
2018-11-14 14:47:16 -0500

    Implement session utils and session actions.

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,41 @@
+import * as APIUtil from '../util/session_api_util';
+
+export const RECEIVE_USER = 'RECEIVE_USER';
+export const LOGOUT_USER = 'LOGOUT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+export const receiveUser = receiveUser => ({
+  type: RECEIVE_USER,
+  currentUser
+});
+
+export const logoutUser = () => ({
+  type: RECEIVE_USER,
+});
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors
+});
+
+export const signup = user => dispatch => (
+  APIUtil.signup(user).then(user => (
+    dispatch(receiveUser(user))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
+);
+
+export const login = user => dispatch => (
+  APIUtil.login(user).then(user => (
+    dispatch(receiveUser(user))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
+);
+
+export const logout = () => dispatch => (
+  APIUtil.logout().then(user => (
+    dispatch(logoutUser())
+  ))
+);
