b3455d0090766299fdb7682aa9fd0a575772de5e
James Lo <lo.jamess@gmail.com>
2018-11-19 03:52:23 -0500

    Import icon for business categories, implement randomized background screen on splash, style splash background image container.

--- frontend/components/splash/splash_page.jsx
+++ frontend/components/splash/splash_page.jsx
@@ -5,11 +5,24 @@ import Footer from '../footer/footer.jsx';
 import SplashSearchNavBar from './splash_search_nav_bar'
 
 
+const randomImage = () => {
+  const images = ["Bear-Bar_by_Ken-L", "Hanami-Sushi_by_Kevin-P",
+    "Union-Square-Market_by_Asim-F", "Mediterani_by_Kristy-K", "Bake-Shack_by_Hope-P",
+    "The-Market_by_Jacqueline-M", "Cheshire_by_Natalia-K", "Tasty-Wok_by_Trung-N",
+    "Tufino-Pizzeria-Napoletana_by_Bradley-H"];
+  const imageStr = images[Math.floor(Math.random() * images.length)];
+  const location = imageStr.split("_by_")[0].split("-").join(" ");
+  const photographer = imageStr.split("_by_")[1].split("-").join(" ");
+  return {image: imageStr, location: location, photographer: photographer};
+}
+
 const SplashPage = () => {
+  const image = randomImage();
+
   return (
     <>
-      <div className='splash-container-a-background'>
-        <div className='splash-container-a'>
+      <div className='splash-container-main-background' style={{backgroundImage: `url(${image.image}.jpg)`}}>
+        <div className='splash-container-main'>
           <SplashHeader />
           <div className='splash-container-inner'>
             <div className='splash-logo-container'>
@@ -20,11 +33,16 @@ const SplashPage = () => {
             </div>
             <SplashSearchNavBar />
           </div>
+          <div className="splash-background-business">
+            {image.location}
+          </div>
+          <div className="splash-background-photographer">
+            Photo by <strong>{image.photographer}.</strong>
+          </div>
         </div>
       </div>
 
-      <div className='splash-container-c'>
-
+      <div className='splash-business-categories-container'>
       </div>
 
       <div className='main-footer-container'>
