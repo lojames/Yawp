50c0f289506ef33d790a0d6c54041c222626913c
James Lo <lo.jamess@gmail.com>
2019-03-06 12:09:39 -0500

    Small bugfix with return in cookie.

--- frontend/util/cookies.js
+++ frontend/util/cookies.js
@@ -1,6 +1,6 @@
-export const getCookie = (name) => {
+export const getCookie = (name) => ()
   document.cookie.split(name+"=")[1].split(";")[0]
-}
+)
 
 export const setCookie = (name, value) => {
   document.cookie = `${name} = ${value}`;
