0c75e8fd58f8445d0743a8c54f6821ce7b07389d
James Lo <lo.jamess@gmail.com>
2019-03-06 12:03:47 -0500

    Fix cookie string splitting.

--- frontend/util/cookies.js
+++ frontend/util/cookies.js
@@ -1,8 +1,5 @@
 export const getCookie = (name) => {
-  const regexp = new RegExp("(?<="+name+"=).*?(?=;)|(?<="+name+"=).*?$");
-  const result = regexp.exec(document.cookie);
-  document.cookie.split(name+"=")[0].split(";")[0]
-  return (result === null) ? null : result[0];
+  document.cookie.split(name+"=")[1].split(";")[0]
 }
 
 export const setCookie = (name, value) => {
